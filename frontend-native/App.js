6313debe672d62e10f8f7f25d4f799c0aab5fe4c
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-01-12 16:55:56 +0200

    Changed date formating

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -39,7 +39,7 @@ export default function App() {
 			console.log(parsedCal.events);
 			let tempArr = []
 			for (const calEvent of parsedCal.events) {
-				tempArr.push({text: calEvent.categories.value, time: calEvent.dtend.value.toString(), completed: false});
+				tempArr.push({text: calEvent.categories.value, time: calEvent.dtend.value.toString().slice(0, -18), completed: false});
 				console.log(calEvent);
 			}
 			setTodoItems([...todoItems, ...tempArr]);
