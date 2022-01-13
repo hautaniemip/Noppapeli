ef0c0da0e8eccf18ef46bf49652afab9b9598cde
Pekka Hautaniemi <hautaniemip@gmail.com>
2022-01-14 01:28:09 +0200

    Moved RollDice to NoppaScreen

--- frontend-native/src/component/NoppaScreen.js
+++ frontend-native/src/component/NoppaScreen.js
@@ -1,9 +1,47 @@
-import RollDice from "../../RollDice";
+import React from 'react'
+import { TouchableOpacity, Text, View, StatusBar, Image } from 'react-native';
+import { globalStyles } from '../../styles/global';
+
+import Dice1 from '../../assets/images/dice1.png';
+import Dice2 from '../../assets/images/dice2.png';
+import Dice3 from '../../assets/images/dice3.png';
+import Dice4 from '../../assets/images/dice4.png';
+import Dice5 from '../../assets/images/dice5.png';
+import Dice6 from '../../assets/images/dice6.png';
 
 
+export default function RollDice() {
+	const [uri, setUri] = React.useState(Dice1);
 
-export default function NoppaScreen() {
-    return (
-        RollDice()
-    );
-}
+	function rollDice() {
+		let rand = Math.floor(Math.random() * 6) + 1;
+
+		switch(rand) {
+			case 1: setUri(Dice1);
+				break;
+			case 2: setUri(Dice2);
+				break;
+			case 3: setUri(Dice3);
+				break;
+			case 4: setUri(Dice4);
+				break;
+			case 5: setUri(Dice5);
+				break;
+			case 6: setUri(Dice6);
+				break;
+			default: setUri(Dice1);
+		}
+	}
+
+	return(
+    <View style={globalStyles.diceContainer}>
+      <Image 
+        style={globalStyles.diceImage} 
+        source={uri}
+      />
+      <TouchableOpacity onPress={rollDice}>
+        <Text style={globalStyles.diceTexts}>Roll the Dice</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
