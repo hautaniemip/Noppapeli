fab73a337408a29ffdcd96d68e4d5343505636d3
Pekka Hautaniemi <hautaniemip@gmail.com>
2022-01-14 01:21:07 +0200

    SocialScreen coming soon...

--- frontend-native/src/component/SocialScreen.js
+++ frontend-native/src/component/SocialScreen.js
@@ -4,8 +4,8 @@ import { Text, SafeAreaView } from 'react-native';
 
 export default function SocialScreen({ navigation }) {
     return (
-        <SafeAreaView>
-            <Text>???</Text>
+        <SafeAreaView style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+			<Text style={{fontSize: 24, textAlign: 'center'}}>Coming Soon...</Text>
         </SafeAreaView>
     );
 }
