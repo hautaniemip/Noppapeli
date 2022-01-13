efe9b88eacae3978a447c03c3cc895a9e4507c62
Zikgo <79146202+Zikgo@users.noreply.github.com>
2022-01-13 16:06:04 +0200

    Navigaatio 1.0

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TouchableOpacity, StyleSheet, Text, SafeAreaView, FlatList, View, Button, StatusBar } from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
 import FileSystem from 'react-native-fs';
@@ -6,17 +8,59 @@ import DeviceInfo from 'react-native-device-info';
 import CalParser from 'cal-parser';
 import TodoInput from './src/component/TodoInput';
 import TodoItem from './src/component/TodoItem';
+import RollDice from './RollDice';
 
 export default function App() {
 
 	const [todoItems, setTodoItems] = React.useState([{text: "Buy groceries", time: "123456", completed: true}, {text: "Make blogpost", time: "12343221", completed: false}])
-
+	const Tab = createBottomTabNavigator();
 
 	function addTodoItem(_text)  {
 		if (_text) {
 			setTodoItems([...todoItems, {text: _text, time: "0", completed: false}]);
 		}
 	}
+
+	  const FriendsScreen = ({ navigation }) => {
+		return (
+			<SafeAreaView>
+				<text>???</text>
+			</SafeAreaView>
+		);
+	  };
+	  const NoppaScreen = ({ navigation }) => {
+		return (
+			RollDice()
+		);
+	  };
+	  function CalenderScreen({ navigation }) {
+		return (
+		<>
+			<StatusBar barStyle={"light-content"} backgroundColor={"#212121"}/>
+			<SafeAreaView style={{padding: 16, flex: 1}}>
+				<Text style={{fontSize: 36, fontWeight: 'bold'}}>Todo</Text>
+				<FlatList
+					data={todoItems}
+					style={{flex: 1}}
+					keyExtractor={(item, index) => index.toString()}
+					renderItem={({item, index}) => {
+						return (
+							<TodoItem
+								item={item}
+								deleteFunction={() => deleteTodoItem(index)}
+								completeFunction={() => completeTodoItem(index)}
+							/>
+						)
+					}}
+				/>
+				<TodoInput onPress={addTodoItem} />
+				<TouchableOpacity onPress={pickFile}>
+					<Text>Pick File...</Text>
+				</TouchableOpacity>
+			</SafeAreaView>
+		</>
+		);
+	  }
 
 	function deleteTodoItem(_index) {
 		let tempArr = [...todoItems];
@@ -53,30 +97,19 @@ export default function App() {
 	}
 
 	return (
-		<>
-			<StatusBar barStyle={"light-content"} backgroundColor={"#212121"}/>
-			<SafeAreaView style={{padding: 16, flex: 1}}>
-				<Text style={{fontSize: 36, fontWeight: 'bold'}}>Todo</Text>
-				<FlatList
-					data={todoItems}
-					style={{flex: 1}}
-					keyExtractor={(item, index) => index.toString()}
-					renderItem={({item, index}) => {
-						return (
-							<TodoItem
-								item={item}
-								deleteFunction={() => deleteTodoItem(index)}
-								completeFunction={() => completeTodoItem(index)}
-							/>
-						)
-					}}
-				/>
-				<TodoInput onPress={addTodoItem} />
-				<TouchableOpacity onPress={pickFile}>
-					<Text>Pick File...</Text>
-				</TouchableOpacity>
-			</SafeAreaView>
-		</>
+		<NavigationContainer >
+			<Tab.Navigator initialRouteName="Noppa">
+			  <Tab.Screen
+				name="Kaverit"
+				component={FriendsScreen}
+			  />
+			  <Tab.Screen 
+			  name="Noppa" 
+			  component={NoppaScreen}
+			   />
+			  <Tab.Screen name="Kalenteri" component={CalenderScreen}/>
+			</Tab.Navigator>
+		</NavigationContainer>
 	);
 }
 
